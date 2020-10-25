40b39f7f5b16d68c7339b056f874ccf28f9c5e86
jholty66 <burholtcharlie@gmail.com>
2020-10-25 15:36:25 +0000

    fixed chroot problem

--- Install/setup.sh
+++ Install/setup.sh
@@ -2,11 +2,5 @@ GITDIR=$(pwd)
 DISTRO=arch
 pacstrap /mnt/$DISTRO base base-devel linux linux-firmware emacs man man-db man-pages texinfo btrfs dosfstools
 genfstab -U /mnt/$DISTRO >> /mnt/$DISTRO/etc/fstab
-cd /mnt/$DISTRO
-mount -t proc none proc
-mount --rbind /sys sys
-mount --make-rslave sys
-mount --rbind /dev dev
-mount --make-rslave dev
-chroot /mnt/$DISTRO chroot.sh
-cd $GITDIR
+cp $GITDIR/chroot.sh /mnt/$DISTRO/root/chroot.sh
+arch-chroot /mnt/$DISTRO /root/chroot.sh

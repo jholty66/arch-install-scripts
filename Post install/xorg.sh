84612cf3ad7894ffcbe0e238d6bacb3e4f068d09
jholty66 <burholtcharlie@gmail.com>
2020-10-30 21:45:17 +0000

    Added fonts and fixed typo.

--- Post install/xorg.sh
+++ Post install/xorg.sh
@@ -4,28 +4,31 @@ sudo pacman -S \
      xorg-server \
      xorg-xinit \
      xorg-xmodmap \
-     xsecurelock
+     xsecurelock 
 
-yay -S terminus-font ttf-go-mono ttf-iosevka ttf-cmu-typewriter xorg-mkfontscale xorg-font-utils
+yay -S terminus-font ttf-go-mono ttf-iosevka ttf-cmu-typewriter xorg-mkfontscale xorg-font-utils ttf-symbola ttf-dejavu adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts
+
+mkdir ~/.config/fontconfig/
+cp fonts.conf ~/.config/fontconfig/
 
 touch ~/.Xresources
-echo "Xft.dpi 120" >> ~/.Xresources
+echo "Xft.dpi: 96 >> ~/.Xresources
 
-touch ~/.xinitrc
-cat <<EOM >~/.xinitrc
-if [ -s ~/.Xresources ]; then
-    xrdb ~/.Xresources
-fi
-xset r rate 250 50 
-echo "exec emacs" >> ~/.xinitrc
-EOM
+# touch ~/.xinitrc
+# cat <<EOM >~/.xinitrc
+# if [ -s ~/.Xresources ]; then
+#     xrdb ~/.Xresources
+# fi
+# xset r rate 250 50 
+# echo "exec emacs" >> ~/.xinitrc
+# EOM
 
-if [ -f ~/.bash_profile]; then
-    rm ~/.bash_profile
+if [ -f ~/.bash_profile ]; then
+    echo ". ~/.profile" >> ~/.bash_profile
 fi
 echo ". ~/.profile" > ~/.bash_profile
 
-if [ -f .profile]; then
+if [ -f .profile ]; then
     rm ~/.profile
 fi
 cat <<EOF

b84543d31ca7e521018edb62f0eaa8a828d954f1
jholty66 <burholtcharlie@gmail.com>
2021-02-03 15:24:57 +0000

    fix

--- custom.sh
+++ custom.sh
@@ -1,20 +1,20 @@
-# Alias.
-cp='cp -v'
-mv='mv -v'
-rm='mm -v'
-grep='grep --colour=auto'
 # Veriables.
 BOOTLOADER="systemd" # "systemd"
-EDITOR="nano"
-EFI_DISK="/dev/nvme0n1" # Disk name.
-EFI_PARTITION="p1" # Partition.
+EDITOR="gvim"
+EFI_DISK="/dev/sda" # Disk name.
+EFI_PARTITION="1" # Partition.
 FS="zfs"
-FS_ROOT="zroot/arch" # Bootfs name, recuired for BTRFS and ZFS.
+FS_ROOT="server/arch" # Bootfs name, recuired for BTRFS and ZFS.
+HOSTNAME="server-arch"
 KERNEL="linux" # "linux", "linux-lts"
 KERNEL_PARAMS=""
+KEYMAP="uk" # blank defaults us
+LOCALE="en_GB.UTF-8"
 PACKSTRAP_PKGS=""
-RAMDISK_OPTS="base systemd sd-vconsole modconf keyboard block filesystems"
+RAMDISK_OPTS="base udev autodetect modconf block keyboard zfs filesystems"
 SERVICES="dhcpcd"
 # Hooks, define function then add to hook.
+BOOTSRAP_PRE_HOOK=""
+BOOTSRAP_POST_HOOK=""
 RAMDISK_PRE_HOOK=""
 RAMDISK_POST_HOOK=""
